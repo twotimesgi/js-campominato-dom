b57144c424158630cb508f9ec0427ca6d9021d92
twotimesgi <luigibardellagerbi@mail.com>
2022-03-03 18:42:14 +0100

    Fix

--- js/script.js
+++ js/script.js
@@ -4,28 +4,79 @@ let text = document.querySelector(".text");
 let field = document.querySelector(".field");
 let cellNumber = [49,81,100];
 let bombs;
+let bombsAround;
+let cells;
+
+function generateGrid(n){
+    for(let y = 0; y <= Math.sqrt(cellNumber[difficultyElem.value] - 1); y++){
+        for (let x = 0; x <= Math.sqrt(cellNumber[difficultyElem.value] - 1); x++){
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.addEventListener("click", checkAround);
+            cell.dataset.x = x;
+            cell.dataset.y = y;
+            field.append(cell);
+        }
+    }
+    cells = document.getElementsByClassName("cell");
+
+}
 
 function generateBombs(){
     bombs = [];
-    while(bombs.length <= 16){
-        let randN = Math.floor(Math.random() * cellNumber[difficultyElem.value] + 1);
-        if(!bombs.includes(randN)) bombs.push(randN);
+    while(bombs.length < 2){
+        let randX = Math.floor(Math.random() * Math.sqrt(cellNumber[difficultyElem.value]));
+        let randY =  Math.floor(Math.random() * Math.sqrt(cellNumber[difficultyElem.value]));
+        if(!isBomb(randX,randY)) bombs.push([randX, randY]);
     }
     console.log(bombs);
 }
 
-function clickCell(){
-    this.classList.add("clicked");
-    if(this.dataset.bomb){
-        console.log("Game Over");
-        let bombCells = document.querySelectorAll('[data-bomb="true"]');
-        for(let i = 0; i < bombs.length - 1; i++){
-            bombCells[i].classList.add("bomb");
+function checkAround(){
+        console.log("click: " + this.dataset.x + ", " + this.dataset.y);
+        if(isBomb(this.dataset.x, this.dataset.y)){
+            this.classList.add("bomb");
+            gameDisable();
+            console.log("hai perso!");
+        }else{
+            this.classList.add("clicked");
+            bombsAround = 0;
+            for(let y = parseInt(this.dataset.y) - 1; y <= parseInt(this.dataset.y) + 1; y++ ){
+                for(let x = parseInt(this.dataset.x) - 1; x <= parseInt(this.dataset.x) + 1; x++){
+                    if(isBomb(x,y)) bombsAround++;
+                }
+            }
+            this.innerHTML = bombsAround;
+            if(matchEnded()){
+                console.log("Hai vinto!");
+                gameDisable();   
+            }
         }
-        let cells = document.querySelectorAll(".cell");
-        for(let i = 0; i < cells.length - 1; i++){
-            cells[i].removeEventListener("click", clickCell);
+        
+}
+
+function matchEnded(){
+    let ended = true;
+    for(let i = 0; i < cells.length - 1; i++){
+        if(!cells[i].classList.contains("clicked") && !isBomb(cells[i].dataset.x ,cells[i].dataset.y)){
+            ended  = false;
         }
+    }
+    console.log("End " + ended);
+    return ended;
+}
+
+function isBomb(x,y){
+    check = false;
+    for(let i = 0; i < bombs.length; i++){
+        if(bombs[i][0] == x && bombs[i][1] == y) check = true;
+    }
+    return check;
+}
+
+function gameDisable(){
+    for(let i = 0; i < cells.length - 1; i++){
+        cells[i].removeEventListener("click", checkAround);
     }
 }
 
@@ -34,13 +85,7 @@ playBtn.addEventListener("click",function(){
     field.classList.add("field-borders");
     text.style.display = "none";
     field.dataset.diff = difficultyElem.value;
+
+    generateGrid(cellNumber[difficultyElem.value]);
     generateBombs();
-    
-    for(let i = 0; i < cellNumber[difficultyElem.value]; i++){
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        cell.addEventListener("click", clickCell);
-        if(bombs.includes(i)) cell.dataset.bomb = true;
-        field.append(cell);
-    }
 });
